57e09467b998ccd83cd055f03eb3babb9b4ff45d
maria luiza goulart <marialuizagp.senai@gmail.com>
2024-03-07 15:28:43 -0300

    up

--- projeto/ToInto/back/recuperar_banco.py
+++ projeto/ToInto/back/recuperar_banco.py
@@ -5,7 +5,7 @@ def selecionar_informacao_cad():
     conex = conexao.conectar()
     cursor = conex.cursor()
 
-    sql = "SELECT  FROM CADASTRO WHERE ID_CAD = %s"
+    sql = "SELECT ID_CAD, NOME_USUARIO, EMAIL, SENHA, FOTO_PERFIL FROM CADASTRO WHERE ID_CAD = %s"
     val = (id_cad,)
     cursor.execute(sql, val)
     usuario = cursor.fetchone()
@@ -21,3 +21,27 @@ def selecionar_informacao_cad():
     
 if __name__ == "__main__":
     selecionar_informacao_cad()
+
+import conexao
+def verificar_informacao_log():
+    id_cad = int(input("Digite o ID do cadastro que deseja selecionar:"))
+
+    conex = conexao.conectar()
+    cursor = conex.cursor()
+
+    sql = "SELECT EMAIL, SENHA FROM CADASTRO WHERE ID_CAD = %s"
+    val = (id_cad,)
+    cursor.execute(sql, val)
+    login = cursor.fetchone()
+    conex.close()
+
+    if login:
+        print("Usuário encontrado:")
+        print(login)
+        return login
+    else:
+        print("Usuario não encontrado")  
+        return None
+    
+if __name__ == "__main__":
+    selecionar_informacao_log()
