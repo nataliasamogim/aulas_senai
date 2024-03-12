556ac2fe9cf339744e985a4fa6aa42bcbf55f00b
maria luiza goulart <marialuizagp.senai@gmail.com>
2024-03-12 11:26:11 -0300

    Criação do componente DELETE

---
+++ projeto/ToInto/back/delete_banco.py
@@ -0,0 +1,12 @@
+import conexao
+
+def deletar_cad(cad_id):
+    conex = conexao.conectar()
+    cursor = conex.cursor()
+
+    sql = "DELETE FROM CADASTRO WHERE ID_CAD = %s"
+    val = (cad_id,)
+    cursor.execute(sql, val)
+    conex.commit()
+    print("Usuário deletado com sucesso!")
+    conex.close()
