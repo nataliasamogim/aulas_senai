c4540483cb3a0701600e4d3bf1eb83514028aa42
laura figueiredo <lauraffigueiredo@gmail.com>
2024-03-19 11:30:10 -0300

    atualização do login

--- projeto/ToInto/front/src/pages/LoginLP/Login.jsx
+++ projeto/ToInto/front/src/pages/LoginLP/Login.jsx
@@ -1,11 +1,11 @@
-{/*Utiliza o useState para a criação de um estado local chamado formValues(vai armazenar as informações do campo de email e senha) */}
+{/*Utiliza o useState para a criação de um estado local chamado formValues(vai armazenar as informações do campo de email e senha) */ }
 {/* Nome do componente: Login*/ }
 {/* Autor(a): Maria Luiza, Laura e Marília */ }
 {/* Data de criação:25/10/2023 e data de alteração: 14/03/2024*/ }
 {/*Representa os campos de email e senha, além do recuperação de senha e botão de cadastrar e entrar do formulário do login*/ }
 {/*Observação pertinente: o estado local armazena as informações recebida do formulário e os handle cuidam da sua interação */ }
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Login.css'
 import { useNavigate } from 'react-router-dom'; //recuperar a rota 
 
@@ -50,7 +50,7 @@ const Login = ({ onLogin }) => {
         // Exibe mensagens de erro no console.log ou em algum local visível
         console.error('Erro no servidor:', resultado.mensagens);
 
-        
+
         // Atualiza o estado com as mensagens de erro para exibição no formulário
         setMensagensErro(resultado.mensagens);
       } else {
@@ -58,7 +58,7 @@ const Login = ({ onLogin }) => {
         console.log('Dados processados com sucesso!', resposta);
         localStorage.setItem('username', resultado.username);
         localStorage.setItem('ID', resultado.id);
-        onLogin(resultado.username); // Chama a função onLogin com o nome de usuário retornado
+        //onLogin(resultado.username); // Chama a função onLogin com o nome de usuário retornado
         //Navega para a tela de calendario
         navigate('/calendario')
       }
@@ -67,51 +67,51 @@ const Login = ({ onLogin }) => {
     }
   };
 
-    return (
+  return (
 
-      <div className="form-container">
+    <div className="form-container">
 
-        {mensagensErro.length > 0 && (
-          <div style={{ color: 'white' }}>
-            <p>Erro ao processar os dados:</p>
-            <ul>
-              {mensagensErro.map((mensagem, index) => (
-                <li key={index}>{mensagem.mensagem}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-
-        <div className='body_log'>
-          <form id="login" onSubmit={handleSubmit}> {/*Logir que possui o email e senha do usuario */}
-            <h1 className="h1_login">Login</h1>
-
-            <div className="email_login">
-              <label className="label_email">Email</label>
-              <input className="input_email" type="email" name="email_log" id="email" value={formValues.email_log} onChange={handleChange} data-email-validate data-required />
-            </div>
-
-            <div className="senha_login">
-              <label className="label_senha">Senha</label>
-              <input className="input_senha" type="password" name="senha_log" id="senha" value={formValues.senha_log} onChange={handleChange} data-password-validate data-required />
-            </div>
-            <p className="paragrafo">Esqueceu a senha?</p>
-
-            <div className="button_log">
-              <div className="log_entrar"> {/*Rota para o calendario */}
-                {/*<Link to="/calendario" className="submit_btn-log" id="btn_logar">Entrar</Link>*/}
-                <input type="submit" value="Entrar" className='submit_btn-log' id='btn_logar' />
-
-              </div>
-              <div className="log_cancel"> {/*Link para o botão cancelar */}
-                {/*<Link className="submit_btn-log" id="btn_cancelar" onclick="limpaForm()">Cancelar</Link>*/}
-                <input type="submit" value="Cancelar" className='submit_btn-log' id='btn_cancelar' onClick="limpaForm()" />
-              </div>
-            </div>
-          </form>
+      {mensagensErro.length > 0 && (
+        <div style={{ color: 'white' }}>
+          <p>Erro ao processar os dados:</p>
+          <ul>
+            {mensagensErro.map((mensagem, index) => (
+              <li key={index}>{mensagem.mensagem}</li>
+            ))}
+          </ul>
         </div>
+      )}
+
+      <div className='body_log'>
+        <form id="login" onSubmit={handleSubmit}> {/*Logir que possui o email e senha do usuario */}
+          <h1 className="h1_login">Login</h1>
+
+          <div className="email_login">
+            <label className="label_email">Email</label>
+            <input className="input_email" type="email" name="email_log" id="email" value={formValues.email_log} onChange={handleChange} data-email-validate data-required />
+          </div>
+
+          <div className="senha_login">
+            <label className="label_senha">Senha</label>
+            <input className="input_senha" type="password" name="senha_log" id="senha" value={formValues.senha_log} onChange={handleChange} data-password-validate data-required />
+          </div>
+          <p className="paragrafo">Esqueceu a senha?</p>
+
+          <div className="button_log">
+            <div className="log_entrar"> {/*Rota para o calendario */}
+              {/*<Link to="/calendario" className="submit_btn-log" id="btn_logar">Entrar</Link>*/}
+              <input type="submit" value="Entrar" className='submit_btn-log' id='btn_logar' />
+
+            </div>
+            <div className="log_cancel"> {/*Link para o botão cancelar */}
+              {/*<Link className="submit_btn-log" id="btn_cancelar" onclick="limpaForm()">Cancelar</Link>*/}
+              <input type="reset" value="Cancelar" className='submit_btn-log' id='btn_cancelar'/>
+            </div>
+          </div>
+        </form>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
