c3ef04b602d7956c5f3e6202e543edd606dfd7c0
laura figueiredo <lauraffigueiredo@gmail.com>
2024-03-19 13:44:58 -0300

    Novas rotas

--- projeto/ToInto/front/src/components/Content.jsx
+++ projeto/ToInto/front/src/components/Content.jsx
@@ -15,6 +15,7 @@ import FormMani from "../pages/backup_pg/FormMani";
 import { PlanoAnual } from "../pages/Escolha_pag/PlanoAnual";
 import { PlanoMensal } from "../pages/Escolha_pag/PlanoMensal"
 import DadosCartao from "../pages/Adic_dados/DadosCartao";
+import Dados_pix from "../pages/Adic_dados/Dados_pix"
 import Modific_cart from "../pages/Modificar_cartao/Modific_cart";
 
 
@@ -33,7 +34,9 @@ const Content = props => (
             <Route path="/mensal" exact element={<PlanoMensal />} />
             <Route path="/modificar" exact element={<FormMani />} />
             <Route path="/dadoscart" exact element={<DadosCartao />} />
+            <Route path="/dadospix" exact element={<Dados_pix />} />
             <Route path="/modificarpag" exact element={<Modific_cart />} />
+
         </Routes>
     </main>
 );
