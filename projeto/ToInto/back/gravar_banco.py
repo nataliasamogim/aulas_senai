9c2f097fc0cea4b23b23cc3bf2175d73606aec88
maria luiza goulart <marialuizagp.senai@gmail.com>
2024-06-06 09:26:03 -0300

    Modificação do insert do cadastro e criação do insert do compromissos

--- projeto/ToInto/back/gravar_banco.py
+++ projeto/ToInto/back/gravar_banco.py
@@ -5,8 +5,36 @@ def gravar_dados(dados_gravacao):
     cursor = conex.cursor()
 
     sql = "INSERT INTO cadastro (NOME_USUARIO, EMAIL, SENHA) VALUES (%s, %s, %s)"
+    nome, email, senha, opc = dados_gravacao
+    val = (nome, email, senha)
+    cursor.execute(sql, val)
+    conex.commit()
+
+    if opc == 1:
+        #nome, email, senha = dados_gravacao
+        sql = "SELECT ID_CAD FROM CADASTRO WHERE NOME_USUARIO = %s AND EMAIL = %s AND SENHA = %s"
+        #val = (nome, email, senha)
+        cursor.execute(sql, val)
+        id = cursor.fetchone()
+        print(id)
+	
+        sql = "INSERT INTO compras (ID_PLANO, ID_DADOS_PAG, ID_CAD, DESC_COMPRA, VALOR_COMPRA) VALUES ('3', NULL , %s, 'Comprou o Plano Grátis', 0.00)"
+
+        teste = cursor.execute(sql, id)
+        print(teste)
+        conex.commit()
+        
+
+    print("Dados do cadastro inseridos com sucesso!") 
+    conex.close()
+
+def gravar_dados_compromisso(dados_gravacao):
+    conex = conexao.conectar()
+    cursor = conex.cursor()
+
+    sql = "INSERT INTO compromissos (ID_CAD, TITULO_COMP, DATA_COMP, HORARIO_COMP, DESCRICAO, IMPORTANTE, LEMBRETE) VALUES (%s, %s, %s, %s, %s, %s, %s)"
 
     cursor.execute(sql, dados_gravacao)
     conex.commit()
-    print("Dados do cadastro inseridos com sucesso!") 
+    print("Dados do compromisso inseridos com sucesso!") 
     conex.close()
