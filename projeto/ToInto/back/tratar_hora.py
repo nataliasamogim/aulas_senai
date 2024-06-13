14459970718b12e10de782ae3df211a0e9e453d2
julia dias <juliadiassenai@gmail.com>
2024-06-13 14:09:13 -0300

    Para tratar data
    Antes a data estava com "/" e tivemos que mudar para "-" , no padrão int

--- projeto/ToInto/back/tratar_hora.py
+++ projeto/ToInto/back/tratar_hora.py
@@ -1,4 +1,3 @@
-# Importa o módulo datetime, que fornece classes para manipulação de datas e horas.
 import datetime
 
 # Define uma função chamada 'data'.
@@ -9,7 +8,7 @@ def data():
     # Obtém a data atual.
     data_atual = datetime.date.today()
     # Formata a data no formato desejado e a retorna.
-    return data_atual.strftime('%d/%m/%Y')
+    return data_atual.strftime('%Y-%m-%d')
 
 # Define uma função chamada 'hora'.
 def hora():
@@ -21,9 +20,5 @@ def hora():
     # Formata o horário no formato desejado e o retorna.
     return hora_atual.strftime('%H:%M:%S')
 
-# Verifica se este arquivo está sendo executado diretamente.
-if __name__ == "__main__":
-    # Imprime a data atual usando a função 'data()'.
-    print("Testando a função 'data()':", data())
-    # Imprime o horário atual usando a função 'hora()'.
-    print("Testando a função 'hora()':", hora())
+
+
