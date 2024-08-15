5ec98047d9da34622a08d101e947380ad6930775
natalia samogim <samogimnatalia@gmail.com>
2024-08-15 15:32:55 -0300

    Recuperação de senha
    Página de recuperação de senha

---
+++ projeto/ToInto/front/src/pages/Esq_senha/Recuperacao.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Recuperacao.css';
+
+function Recuperacao() {
+    const [formValues, setFormValues] = useState({ 
+        email: '' 
+    });
+    
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues((prevValues) => ({ ...prevValues, [name]: value }));
+    };
+
+    const [mensagensErro, setMensagensErro] = useState([]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const resposta = await fetch('http://localhost:5000/receber-dados', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formValues),
+            });
+
+            const resultado = await resposta.json();
+
+            if (resultado.erro) {
+                console.error('Erro ao enviar e-mail de recuperação:', resultado.mensagens);
+                setMensagensErro(resultado.mensagens);
+            } else {
+                console.log('E-mail de recuperação enviado', resposta);
+                
+            }
+        } catch (error) {
+            console.error('Erro ao enviar dados:', error);
+        }
+    };
+
+    const limpaForm = () => {
+        setFormValues({ 
+            email: '' 
+        });
+    };
+
+    return (
+        <div className="esqueceu-container">
+            {mensagensErro.length > 0 && (
+                <div style={{ color: 'white' }}>
+                    <p>Erro ao processar os dados:</p>
+                    <ul>
+                        {mensagensErro.map((mensagem, index) => (
+                            <li key={index}>{mensagem.mensagem}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+
+            <section className="form_recuperarsenha">
+                <form className="recuperar_senha" onSubmit={handleSubmit}>
+                    <h1 className="h1_rec_senha">Recuperação de senha:</h1>
+                    <div className="form_grupo">
+                        <p>Digite o e-mail para enviarmos o código:</p>
+                        <input className="input_email" type="email" name="email" placeholder="E-mail" value={formValues.rec_email} onChange={handleChange} data-email-validate data-required/>
+                    </div>
+
+                    <div className="btn_rec_senha">
+                        <div className="salvar_rec_senha">
+                            <input type='submit' className="btn_recsenha" value="Salvar" />
+                        </div>
+                        <div className="can_rec_senha">
+                            <input type='button' className="btn_recsenha" onClick={limpaForm} value="Cancelar" />
+                        </div>
+                    </div>
+                </form>
+            </section>
+        </div>
+    );
+}
+
+export default Recuperacao;
