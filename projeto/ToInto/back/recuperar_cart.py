0e98b124b162121e2d59e580511007f887081900
maria luiza goulart <marialuizagp.senai@gmail.com>
2024-08-20 08:49:10 -0300

    Atualização do select da data do formulario do dados do cartão

--- projeto/ToInto/back/recuperar_cart.py
+++ projeto/ToInto/back/recuperar_cart.py
@@ -4,12 +4,12 @@ def recuperar_inf_cart(id_cad):
     conex = conexao.conectar()
     cursor = conex.cursor()
 
-    sql = "SELECT ID_DADOS_PAG, TIPO_PAG, CPF, DATA_PAG, NUM_CARTAO, CVV, DATA_VENC, NOME_CARTAO FROM DADOS_PAG WHERE ID_CAD = %s"
+    sql = 'SELECT ID_DADOS_PAG, TIPO_PAG, CPF, DATA_PAG, NUM_CARTAO, CVV, DATE_FORMAT(DATA_VENC,"%Y-%m-%d") as DATA_VENC, NOME_CARTAO FROM DADOS_PAG WHERE ID_CAD = %s order by ID_DADOS_PAG desc LIMIT 1'
     val = (id_cad,)
-    print(id_cad)
     cursor.execute(sql, val)
     cartao = cursor.fetchone()
     conex.close()
+    print(cartao)
 
     if cartao:
         print('Compra do usuário Laura')
