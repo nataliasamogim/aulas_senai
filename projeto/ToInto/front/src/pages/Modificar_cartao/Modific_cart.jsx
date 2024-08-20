4244b58944983106fc6f29277bd4ca7948d5e12a
maria luiza goulart <marialuizagp.senai@gmail.com>
2024-08-20 16:09:03 -0300

    select e update do form de cartão

--- projeto/ToInto/front/src/pages/Modificar_cartao/Modific_cart.jsx
+++ projeto/ToInto/front/src/pages/Modificar_cartao/Modific_cart.jsx
@@ -5,7 +5,7 @@
 lado do cliente, e envia esses dados para um servidor, para ser processado e gravado em um documento txt. O componente é 
 configurado para fornecer feedback visual aos usuários sobre o sucesso ou falha no processamento do formulário.*/}
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Modific_cart.css';
 import { useNavigate } from 'react-router-dom'; //recuperar a rota 
 
@@ -13,67 +13,91 @@ import { useNavigate } from 'react-router-dom'; //recuperar a rota
 cpf, num_cartao, datavenc, cod_segurança) */ }
 const Modific_cart = () => {
     const navigate = useNavigate();
-    const [formValues, setFormValues] = useState({
-        nome_titular: '',
-        cpf: '',
-        num_cartao: '',
-        datavenc: '',
-        cod_seguranca: '',
-    });
+    const [nomeTitular, setNomeTitular] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [numCartao, setNumCartao] = useState('')
+    const [dataVenc, setDataVenc] = useState('')
+    const [codSeguranca, setCodSeguranca] = useState('')
 
+    useEffect(() => {
+        // Função assíncrona para buscar dados do usuário
+        const showDados = async () => {
+            const id_cad = localStorage.getItem('ID')
+            try {
+                // Faz uma requisição para receber os dados do usuário do servidor
+                const resposta = await fetch('http://localhost:5000/receber-dados', {
+                    method: 'POST', // Método da requisição
+                    headers: {
+                        'Content-Type': 'application/json', // Tipo de conteúdo da requisição
+                    },
+                    body: JSON.stringify({ acao: 'selecionar_cart', id_cadastro: id_cad }), // Corpo da requisição contendo os dados do formulário
+                });
 
-    {/* O método handleChange é chamado sempre que um dos campos do formulário é alterado. 
-    Ele atualiza o estado formValues com os novos valores do campo.*/}
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues((prevValues) => ({
-            ...prevValues,
-            [name]: value,
-        }));
-    };
+                // Verifica se a requisição foi bem-sucedida
+                if (!resposta.ok) {
+                    throw new Error('Erro ao obter dados do cartão'); // Lança um erro se a requisição falhar
+                }
+                // Extrai os dados da resposta e os converte para JSON
+                const dadosCart = await resposta.json();
+                console.log(dadosCart)
+                //console.log('Dados do usuário:', userData);
+                console.log("Dados recebidos do select do cartão", dadosCart.mensagem);
+                // Atualiza o estado do formulário com os dados do usuário recebidos
+                setNomeTitular(dadosCart.mensagem[7]); // Define o novo valor para 'nome'
+                setCpf(dadosCart.mensagem[2]);
+                setNumCartao(dadosCart.mensagem[4]);
+                setDataVenc(dadosCart.mensagem[6]);
+                setCodSeguranca(dadosCart.mensagem[5].toString());
+            } catch (error) {
+                console.error('Erro ao buscar dados do cartão 2:', error); // Captura e exibe qualquer erro ocorrido durante a busca dos dados do usuário
+            }
+        };
+
+        showDados(); // Chama a função para buscar os dados do usuário quando o componente é montado
+    }, []); // Array de dependências vazio, indica que este efeito deve ser executado apenas uma vez
 
     const [mensagensErro, setMensagensErro] = useState([]);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
+    const handleAtualizarCart = async (e) => {
+        e.preventDefault(); // Prevenir o envio padrão do formulário
+        const id_cad = localStorage.getItem('ID');
         try {
+            // Faz uma requisição para enviar os dados do formulário para o servidor
             const resposta = await fetch('http://localhost:5000/receber-dados', {
-                method: 'POST',
+                method: 'POST', // Método da requisição
                 headers: {
-                    'Content-Type': 'application/json',
+                    'Content-Type': 'application/json', // Tipo de conteúdo da requisição
                 },
-                body: JSON.stringify(formValues),
+                body: JSON.stringify({
+                    acao: 'atualizar_cart',
+                    id: id_cad,
+                    novo_Cpf: cpf,
+                    novo_numCartao: numCartao,
+                    novo_cvv: codSeguranca,
+                    nova_dataVenc: dataVenc,
+                    novo_nomeTitular: nomeTitular,
+                }), // Corpo da requisição contendo os dados do formulário em formato JSON
             });
-
+            // Extrai o resultado da resposta e o converte para JSON
             const resultado = await resposta.json();
-
+            console.log('teste retorno do cartão', resultado);
+            // Verifica se ocorreu algum erro no servidor
             if (resultado.erro) {
-                // Exibe mensagens de erro no console.log ou em algum local visível
+                // Exibe mensagens de erro no console ou em algum local visível
                 console.error('Erro no servidor:', resultado.mensagens);
-
                 // Atualiza o estado com as mensagens de erro para exibição no formulário
                 setMensagensErro(resultado.mensagens);
             } else {
-                // Dados foram processados com sucesso
-                console.log('Dados processados com sucesso!', resposta);
-
-                //Navega para a tela do  concluído
-                navigate('/modificarpag')
+                console.log('Dados de cartão atualizados com sucesso!', resultado);
+                setNomeTitular('');
+                setCpf('');
+                setNumCartao('');
+                setDataVenc('');
+                setCodSeguranca('');
+                navigate('/calendario')
             }
         } catch (error) {
-            console.error('Erro ao enviar dados:', error);
+            console.error('Erro ao enviar dados do cartão:', error); // Captura e exibe qualquer erro ocorrido durante o envio dos dados do formulário
         }
-    };
-
-    
-    const limpaForm = () => {
-        setFormValues({
-            nome: '',
-            email: '',
-            senha: '', 
-            confirmsenha: '',
-        });
     };
 
     return (
@@ -92,32 +116,32 @@ const Modific_cart = () => {
             )}
 
             <section className="form_modifcartao">
-                <form className="modificar_cartao" id="modificarcart" action="" onSubmit={handleSubmit} /*O método handleSubmit é chamado quando o formulário é enviado.*/> {/*form do cartão*/}
+                <form className="modificar_cartao" id="modificarcart" action="" onSubmit={handleAtualizarCart}/*O método handleSubmit é chamado quando o formulário é enviado.*/> {/*form do cartão*/}
                     <h1 className="h1_modific_cartao">Modificar dados do cartão</h1>
 
                     <div className="form_grupo"> {/*div para parte do nome do titular*/}
                         <label className="nome_titular">Nome completo do titular</label>
-                        <input className="input_1" type="text" name="nome_titular" id="nome_titular" value={formValues.nome_titular} onChange={handleChange} placeholder="Digite o nome do titular" data-min-length="3" data-max-length="100" data-only-letters />
+                        <input className="input_1" type="text" name="nome_titular" id="nome_titular" value={nomeTitular} onChange={(e) => setNomeTitular(e.target.value)} placeholder="Digite o nome do titular" data-min-length="3" data-max-length="100" data-only-letters />
                     </div>
 
                     <div className="form_grupo"> {/*div para parte do cpf*/}
                         <label className="cpf">CPF</label>
-                        <input className="input_2" type="text" name="cpf" id="cpf" value={formValues.cpf} onChange={handleChange} placeholder="Digite o CPF" data-min-length="3" data-max-length="100" data-only-letters />
+                        <input className="input_2" type="text" name="cpf" id="cpf" value={cpf} onChange={(e) => setCpf(e.target.value)} placeholder="Digite o CPF" data-min-length="3" data-max-length="100" data-only-letters />
                     </div>
 
                     <div className="form_grupo"> {/*div para parte do numero do cartao*/}
                         <label className="num_cartao">Número do cartão</label>
-                        <input className="input_3" type="number" name="num_cartao" id="num_cartao" value={formValues.num_cartao} onChange={handleChange} placeholder="Digite o número do cartão" data-min-length="16" data-max-length="40" />
+                        <input className="input_3" type="number" name="num_cartao" id="num_cartao" value={numCartao} onChange={(e) => setNumCartao(e.target.value)} placeholder="Digite o número do cartão" data-min-length="16" data-max-length="40" />
                     </div>
 
                     <div className="form_grupo"> {/*div para parte da data de vencimento*/}
                         <label className="datavenc">Data de vencimento</label>
-                        <input className="input_4" type="date" name="datavenc" id="datavenc" value={formValues.datavenc} onChange={handleChange} placeholder="Digite a data de vencimento" data-min-length="8" data-max-length="15" />
+                        <input className="input_4" type="date" name="datavenc" id="datavenc" value={dataVenc} onChange={(e) => setDataVenc(e.target.value)} placeholder="Digite a data de vencimento" data-min-length="8" data-max-length="15" />
                     </div>
 
                     <div className="form_grupo"> {/*div para a parte de codigo de segurança*/}
                         <label className="codigo_seg">Código de segurança </label>
-                        <input className="input_5" type="number" name="cod_seguranca" id="cod_seguranca" value={formValues.cod_seguranca} onChange={handleChange} placeholder="Digite o código de segurança" data-min-length="3" />
+                        <input className="input_5" type="number" name="cod_seguranca" id="cod_seguranca" value={codSeguranca} onChange={(e) => setCodSeguranca(e.target.value)} placeholder="Digite o código de segurança" data-min-length="3" />
                     </div>
 
                     <div className="btn_modific_cart">
@@ -125,7 +149,7 @@ const Modific_cart = () => {
                             <input type='submit' className="btn_cartao" id="btn_sal_cart" value="Salvar" />
                         </div>
                         <div className="can_modif_cart"> {/*botão cancelar do footer */}
-                            <input type='submit' className="btn_cartao" id="btn_can_cart" onClick={limpaForm} value="Cancelar" />
+                            <input type="button" className="btn_cartao" id="btn_can_cart" value="Cancelar" />
                         </div>
                     </div>
                 </form>
