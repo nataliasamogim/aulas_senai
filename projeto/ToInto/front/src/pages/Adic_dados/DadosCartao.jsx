70661bd5246b72952b9b9a184b37b684be85b6e0
maria luiza goulart <marialuizagp.senai@gmail.com>
2024-08-20 16:09:26 -0300

    Create do form de cartão

--- projeto/ToInto/front/src/pages/Adic_dados/DadosCartao.jsx
+++ projeto/ToInto/front/src/pages/Adic_dados/DadosCartao.jsx
@@ -5,13 +5,14 @@ import './DadosCartao.css'
 {/*Utiliza o useState para a criação de um estado local chamado formValues(vai armazenar as informações do campo de email e senha) */ }
 const DadosCartao = () => {
     const navigate = useNavigate();
-    const [formValues, setFormValues] = useState({
-        nome_titular: '',
+    const [formCartao, setFormCartao] = useState({
+        id: localStorage.getItem("ID"),
         cpf: '',
         num_cartao: '',
-        datavenc: '',
         cod_seguranca: '',
-
+        datavenc: '',
+        nome_titular: '',
+        escolha_pag: '2'
     });
 
 
@@ -19,7 +20,7 @@ const DadosCartao = () => {
 Ele atualiza o estado formValues com os novos valores do campo.*/}
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setFormValues((prevValues) => ({
+        setFormCartao((prevValues) => ({
             ...prevValues,
             [name]: value,
         }));
@@ -29,14 +30,13 @@ Ele atualiza o estado formValues com os novos valores do campo.*/}
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         try {
             const resposta = await fetch('http://localhost:5000/receber-dados', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(formValues),
+                body: JSON.stringify(formCartao),
             });
 
             const resultado = await resposta.json();
@@ -49,8 +49,8 @@ Ele atualiza o estado formValues com os novos valores do campo.*/}
                 setMensagensErro(resultado.mensagens);
             } else {
                 // Dados foram processados com sucesso
-                console.log('Dados processados com sucesso!', resposta);
-
+                console.log('Dados processados do cartão com sucesso!', resposta);
+                setFormCartao('')
                 //Navega para a tela do Cadastro concluído
                 navigate('/concluido')
             }
@@ -80,27 +80,27 @@ Ele atualiza o estado formValues com os novos valores do campo.*/}
 
                     <div className="form_grupo"> {/*div para parte do nome*/}
                         <label className="nome_titular">Nome completo do titular</label>
-                        <input className="input_1" type="text" name="nome_titular" id="nome_titular" value={formValues.nome_titular} onChange={handleChange} placeholder="Digite o nome do titular" data-min-length="3" data-max-length="100" data-only-letters />
+                        <input className="input_1" type="text" name="nome_titular" id="nome_titular" value={formCartao.nome_titular} onChange={handleChange} placeholder="Digite o nome do titular" data-min-length="3" data-max-length="100" data-only-letters />
                     </div>
 
                     <div className="form_grupo"> {/*div para parte do nome*/}
                         <label className="cpf">CPF</label>
-                        <input className="input_2" type="text" name="cpf" id="cpf" value={formValues.cpf} onChange={handleChange} placeholder="___.___.___-__" data-min-length="3" data-max-length="100" data-only-letters />
+                        <input className="input_2" type="text" name="cpf" id="cpf" value={formCartao.cpf} onChange={handleChange} placeholder="___.___.___-__" data-min-length="3" data-max-length="100" data-only-letters />
                     </div>
 
                     <div className="form_grupo"> {/*div para parte do e-mail*/}
                         <label className="num_cartao">Número do cartão</label>
-                        <input className="input_3" type="number" name="num_cartao" id="num_cartao" value={formValues.num_cartao} onChange={handleChange} placeholder="Digite o número do cartão" data-min-length="16" data-max-length="40" />
+                        <input className="input_3" type="number" name="num_cartao" id="num_cartao" value={formCartao.num_cartao} onChange={handleChange} placeholder="Digite o número do cartão" data-min-length="16" data-max-length="40" />
                     </div>
 
                     <div className="form_grupo"> {/*div para parte da senha*/}
                         <label className="datavenc">Data de vencimento</label>
-                        <input className="input_4" type="date" name="datavenc" id="datavenc" value={formValues.datavenc} onChange={handleChange} placeholder="Digite a data de vencimento" data-min-length="8" data-max-length="15" />
+                        <input className="input_4" type="date" name="datavenc" id="datavenc" value={formCartao.datavenc} onChange={handleChange} placeholder="Digite a data de vencimento" data-min-length="8" data-max-length="15" />
                     </div>
 
                     <div className="form_grupo"> {/*div para a parte de confirmar senha*/}
                         <label className="codigo_seg">Código de segurança </label>
-                        <input className="input_5" type="number" name="cod_seguranca" id="cod_seguranca" value={formValues.cod_seguranca} onChange={handleChange} placeholder="Digite o código de segurança" data-min-length="3" />
+                        <input className="input_5" type="number" name="cod_seguranca" id="cod_seguranca" value={formCartao.cod_seguranca} onChange={handleChange} placeholder="Digite o código de segurança" data-min-length="3" />
                     </div>
 
                     <div className="buttons_cartao">
@@ -108,7 +108,7 @@ Ele atualiza o estado formValues com os novos valores do campo.*/}
                             <input type='submit' className="btn_cartao" id="btn_salvarcart" value="Salvar" />
                         </div>
                         <div className="can_cartao"> {/*botão cancelar do footer */}
-                            <input type='submit' className="btn_cartao" id="btn_can_cart" onClick="limpaForm()" value="Cancelar" />
+                            <input type='submit' className="btn_cartao" id="btn_can_cart" value="Cancelar" />
                         </div>
                     </div>
                 </form>
