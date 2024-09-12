873391b82128f85f7a5eefa7465953319e786257
laura figueiredo <lauraffigueiredo@gmail.com>
2024-09-12 14:10:49 -0300

    Delete banco para compromissos

--- projeto/ToInto/back/delete_banco.py
+++ projeto/ToInto/back/delete_banco.py
@@ -11,3 +11,19 @@ def deletar_cad(cad_id):
     print("Usuário deletado com sucesso!")
     conex.close()
     return {'erro': False, 'mensagem': 'Deletado com sucesso'}
+
+def deletar_compromisso(comp_id):
+    try:
+        conex = conexao.conectar()
+        cursor = conex.cursor()
+
+        sql = "DELETE FROM COMPROMISSOS WHERE ID_COMP = %s"
+        val = (comp_id,)  # Certifique-se de que `val` é uma tupla
+        cursor.execute(sql, val)
+        conex.commit()
+    except Exception as e:
+        print(f"Erro ao deletar compromisso: {e}")
+        return {'erro': True, 'mensagem': str(e)}
+    finally:
+        conex.close()
+    return {'erro': False, 'mensagem': 'Deletado com sucesso'}
