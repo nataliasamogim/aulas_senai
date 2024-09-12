fe3fb0209097d9a7ac669d81f8d53d4e6caab989
laura figueiredo <lauraffigueiredo@gmail.com>
2024-09-12 14:04:27 -0300

    Mudanças no Calendar
    Para puxar a data no To Do

--- projeto/ToInto/front/src/components/Calendar/Calendar.jsx
+++ projeto/ToInto/front/src/components/Calendar/Calendar.jsx
@@ -1,32 +1,108 @@
-{/* Componente Calendar*/}
-{/* Autor: Júlia Dias Lara */}
-{/* 13/09/2023-01/12/2023*/}
-{/* Descrição detalhada: o calendar apresenta o mês, ano e eventos associados aos dias, possui funcionalidades de navegação*/}
-{/* entre os meses, inclui a associação dos eventos aos dias, com cada dia oferecendo um link para o To Do List, representado por um "+".*/}
-{/* Observações pertinentes: O código importa o Link do pacote React-router-dom que o leva para o To Do List */}
-
-import React, { useState } from 'react';
-import Event from '../Eventos/Event';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Calendar.css';
+import Compromissos from './../../pages/Compromissos/Compromissos';
+import { useParams } from 'react-router-dom';
 
-{/* Uma lista dos meses do ano que é atribuído a uma constante  */}
 const months = [
   'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
   'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
 ];
 
-{/* Uma lista dos dias da semana que é atribuído a uma constante */}
 const daysOfWeek = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'];
 
 const Calendar = () => {
-    const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
-    const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
-    const [events, setEvents] = useState([]);
-    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
-    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+  const { selectedDate } = useParams();
+  const [date, setDate] = useState(new Date());
+  const [modal, setModal] = useState(false);
+  const [tarefas, setTarefas] = useState([]);
+  const [tarefaData, setTarefaData] = useState([]);
+  const [selecionarTarefa, setSelecionarTarefa] = useState(null);
+  const [currentDate, setCurrentDate] = useState(selectedDate || new Date().toISOString().split('T')[0]);
+  const [checkedTarefas, setCheckedTarefas] = useState({});
 
-  {/*É chamada no código para navegar para o mês anterior no calendário */}
+  const formatodata = !isNaN(date.getTime())
+    ? new Intl.DateTimeFormat('pt-BR', { dateStyle: 'short' }).format(date)
+    : 'Data inválida';
+
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+  const receberTarefas = async () => {
+    try {
+      const resposta = await fetch('http://10.135.60.20:8085/receber-dados', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ acao: 'recuperar_comp', id_cad: localStorage.getItem("ID"), data_comp: currentDate })
+      });
+      const dados = await resposta.json();
+      console.log('Tarefas recuperadas:', dados);
+
+      if (dados && Array.isArray(dados.mensagem)) {
+        const trat_tarefas = dados.mensagem.map(([id_comp, titulo_comp, horario_comp, descricao, importante, lembrete]) => ({
+          id_comp,
+          titulo: titulo_comp,
+          horario: horario_comp,
+          descricao,
+          importante,
+          lembrete,
+          data_comp: currentDate // Adiciona a data da tarefa
+        }));
+        setTarefas(trat_tarefas);
+        setTarefaData(trat_tarefas);
+      }
+      else {
+        setTarefas([]);
+        setTarefaData([]);
+      }
+    } catch (error) {
+      console.error('Erro ao buscar tarefas:', error);
+    }
+  };
+
+  useEffect(() => {
+    receberTarefas();
+  }, [currentDate]);
+
+  const handleDateClick = (day) => {
+    const clickedDate = new Date(currentYear, currentMonth, day + 1);
+
+    if (isNaN(clickedDate.getTime())) {
+      console.error("Data inválida:", clickedDate);
+      return;
+    }
+
+    setDate(clickedDate);
+    const selectDate = clickedDate.toISOString().split('T')[0];
+    setCurrentDate(selectDate);
+
+    const tarefasSelecionarData = tarefas.filter(
+      (tarefa) => tarefa.data_comp === selectDate
+    );
+
+    setTarefaData(tarefasSelecionarData);
+  };
+
+  const handleAddTarefas = () => {
+    setSelecionarTarefa(null);
+    setModal(true);
+  };
+
+  const handleEditTarefa = (tarefa) => {
+    setSelecionarTarefa(tarefa);
+    setModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setModal(false);
+    setSelecionarTarefa(null);
+    receberTarefas();
+  };
+
   const handlePrevMonth = () => {
     if (currentMonth === 0) {
       setCurrentMonth(11);
@@ -36,7 +112,6 @@ const Calendar = () => {
     }
   };
 
-  {/*É chamada no código para navegar para o próximo mês no calendário */}
   const handleNextMonth = () => {
     if (currentMonth === 11) {
       setCurrentMonth(0);
@@ -45,70 +120,145 @@ const Calendar = () => {
       setCurrentMonth(currentMonth + 1);
     }
   };
- 
+
+  const handleCheckBox = (idComp) => {
+    setCheckedTarefas(prevState => ({
+      ...prevState,
+      [idComp]: !prevState[idComp] //Atualiza apenas a tarefa específica com o id_comp
+    }))
+  }
+
+  const handleDelete = async (idComp) => {
+    if (!tarefaData) return;
+
+    try {
+      console.log("tarefaData ", idComp)
+      const response = await fetch('http://10.135.60.20:8085/receber-dados', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          acao: 'deletar_comp',
+          id_comp: idComp
+        })
+      });
+      console.log('id de compromisso:', tarefaData[0].id_comp)
+
+      if (!response.ok) {
+        throw new Error('Erro ao deletar tarefa');
+      }
+      else {
+        console.log('Tarefa deletada com sucesso')
+        receberTarefas();
+      }
+
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  };
 
   return (
     <div className="calendar">
-      {/* Início do cabeçalho do calendário */}
-      <div className="calendar-header">
-        {/* Botão com uma seta que levará ao mês anterior */}
-        <button onClick={handlePrevMonth}>
+      <div className="calendarioTotal">
+        <div className="calendar-header">
+          <button onClick={handlePrevMonth}>
             <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-caret-left" viewBox="0 0 16 16" color="white">
-                <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
+              <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z" />
             </svg>
-        </button>
-
-        {/*Div para o mês e ano atuais */}
-        <div className="mes_ano">
-            <h2>{months[currentMonth]} </h2>
+          </button>
+          <div className="mes_ano">
+            <h2>{months[currentMonth]}</h2>
             <h4>{currentYear}</h4>
-        </div>
-       
-        {/*Botão com uma seta que levará ao próximo mês */}
-        <button onClick={handleNextMonth}>
+          </div>
+          <button onClick={handleNextMonth}>
             <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-caret-right" viewBox="0 0 16 16" color="white">
-                <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z" />
+              <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z" />
             </svg>
+          </button>
+        </div>
+        <div className="calendar-grid">
+          {daysOfWeek.map((day) => (
+            <div key={day} className="day-of-week">
+              {day}
+            </div>
+          ))}
+          {[...Array(firstDayOfMonth).fill(null)].map((_, index) => (
+            <div key={`empty-${index}`} className="empty-day"></div>
+          ))}
+          {[...Array(daysInMonth).keys()].map((day) => (
+            <div key={day} className="day" onClick={() => handleDateClick(day)}>
+              <span className="day-number">{day + 1}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='toDoList'>
+        <p id='DataTodo'>{formatodata}</p>
+        <ul>
+          {tarefaData.length > 0 ? (
+            tarefaData.map((tarefa) => (
+              <li key={tarefa.id_comp}>
+                <div className='tarefa_edit_lixo'>
+                  <div className='check_tarefa'>
+                    <div className='container-check'>
+                      <input
+                        checked={checkedTarefas[tarefa.id_comp] || false}
+                        onClick={() => handleCheckBox(tarefa.id_comp)}
+                        type="checkbox"
+                        name="check"
+                        id="check" />
+                    </div>
+                    <div className='hora-titulo'>
+                      <span className='horario_tarefa'>{tarefa.horario} - </span>
+                      <span className='titulo_tarefa'>{tarefa.titulo}</span>
+                    </div>
+                    
+                  </div>
+
+                  <div id='botoes'>
+
+                  <button className='buttonEditar' onClick={() => handleEditTarefa(tarefa)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16" color="white">
+                      <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+                    </svg>
+                  </button>
+
+                  <button
+                    type="button"
+                    className="buttonExcluir"
+                    onClick={() => handleDelete(tarefa.id_comp)}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16" color="white">
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+                    </svg>
+                  </button>
+                  </div>
+                </div>
+                <span className='descricaoTarefa'>{tarefa.descricao || 'Nenhuma descrição disponível'}</span>
+
+              </li>
+            ))
+          ) : (
+            <li id='nenhuma-task'>Nenhuma tarefa para este dia</li>
+          )}
+        </ul>
+        <button className='butTodo' onClick={handleAddTarefas}>
+          +
         </button>
 
       </div>
-      {/* Fim do cabeçalho do calendário */}
-
-      <div className="calendar-grid">
-        {/*Bloco que mapeia a lista days of week */}
-        {daysOfWeek.map((day) => (
-          <div key={day} className="day-of-week">
-            {day}
-          </div>
-        ))}
-        {/* Bloco que cria divs vazias representando os dias do mês anterior até o primeiro dia do mês atual. */}
-        {[...Array(firstDayOfMonth).fill(null)].map((_, index) => (
-          <div key={`empty-${index}`} className="empty-day"></div>
-        ))}
-        {/* Bloco que cria divs representando os dias do mês atual */}
-        {[...Array(daysInMonth).keys()].map((day) => (
-
-          <div key={day} className="day" onClick={() => handleAddEvent(day)}> {/* Para cada dia do mês, cria uma div com a classe "day". A chave é definida como o número do dia.*/}
-            <span className="day-number">{day + 1}</span>
-            <div className="todoplus">
-                <Link to="/Todolist" className="todo_button">+</Link> {/*Link para redirecionar ao To Do List */}
-            </div>
-            {/* Mapeia os eventos associados a cada dia. */}
-            {events.map((event, index) => {
-              {/*Se houver um evento naquele dia específico, renderiza um componente Event com as informações desse evento. */}
-              if (event.date === day + 1) {
-                return <Event key={index} title={event.title} />;
-              }
-              return null;
-            })}
-          </div>
-        ))}
-      </div>
+      <Compromissos
+        isOpen={modal}
+        onRequestClose={handleCloseModal}
+        tarefasData={selecionarTarefa}
+        receberTarefas={receberTarefas}
+        dataEscolhida={currentDate} // A data fixa que você quer passar
+      />
     </div>
   );
-
-
-
 };
 
 export default Calendar;
