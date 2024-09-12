8f406ed7172045210aacacc789abfd0798db287d
laura figueiredo <lauraffigueiredo@gmail.com>
2024-09-12 14:07:41 -0300

    Mudança no validar comp

--- projeto/ToInto/back/validar_compromissos.py
+++ projeto/ToInto/back/validar_compromissos.py
@@ -1,15 +1,13 @@
 def validar_titulo(titulo):
     if len(titulo) < 3:
         return {'erro': True, 'mensagem': 'O titulo deve ter pelo menos 3 caracteres'}
-    if len(titulo) > 200:
-        return {'erro' : True, 'mensagem': 'O nome deve conter no máximo 200 caracteres'}
+    if len(titulo) > 35:
+        return {'erro' : True, 'mensagem': 'O titulo deve conter no máximo 35 caracteres'}
 
     return {'erro': False, 'mensagem': ''}
 
 def validar_descricao(desc):
-    if len(desc) < 3:
-        return {'erro': True, 'mensagem': 'O nome deve ter pelo menos 3 caracteres'}
-    if len(desc) > 100:
-        return {'erro' : True, 'mensagem': 'O nome deve conter no máximo 100 caracteres'}
+    if len(desc) > 70:
+        return {'erro' : True, 'mensagem': 'A descrição deve conter no máximo 150 caracteres'}
 
     return {'erro': False, 'mensagem': ''}
