abec206f0757b2605bc47d021f9d6747e9558fb7
laura figueiredo <lauraffigueiredo@gmail.com>
2024-09-24 11:10:59 -0300

    Ajuste no importante
    Todas tarefas marcadas como importante ficam azul.

--- projeto/ToInto/front/src/components/Calendar/Calendar.jsx
+++ projeto/ToInto/front/src/components/Calendar/Calendar.jsx
@@ -32,7 +32,7 @@ const Calendar = () => {
 
   const receberTarefas = async () => {
     try {
-      const resposta = await fetch('http://10.135.60.29:8085/receber-dados', {
+      const resposta = await fetch('http://10.135.60.14:8085/receber-dados', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -133,7 +133,7 @@ const Calendar = () => {
 
     try {
       console.log("tarefaData ", idComp)
-      const response = await fetch('http://10.135.60.29:8085/receber-dados', {
+      const response = await fetch('http://10.135.60.14:8085/receber-dados', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -199,7 +199,7 @@ const Calendar = () => {
         <ul>
           {tarefaData.length > 0 ? (
             tarefaData.map((tarefa) => (
-              <li className='licalendar' key={tarefa.id_comp}>
+              <li className={`licalendar ${tarefa.importante ? 'tarefa-importante' : ''}`} key={tarefa.id_comp}>
                 <div className='tarefa_edit_lixo'>
                   <div className='check_tarefa'>
                     <div className='container-check'>
