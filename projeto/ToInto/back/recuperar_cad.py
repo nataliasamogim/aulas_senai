1d8154fe1a4283aa157b765a9c55f381d3bb8fa7
maria luiza goulart <marialuizagp.senai@gmail.com>
2024-09-24 15:53:54 -0300

    Alteração do select do login para trazer o plano escolhido também

--- projeto/ToInto/back/recuperar_cad.py
+++ projeto/ToInto/back/recuperar_cad.py
@@ -12,6 +12,7 @@ def recuperar_inf_formani(id_cad):
     conex.close()
 
     if usuario:
+        print('teste de recebimento dos dados do usuário', usuario)
         return {'erro': False, 'mensagem': usuario}
 
     else:
@@ -22,13 +23,14 @@ def verificar_informacao_log(email, senha):
     conex = conexao.conectar()
     cursor = conex.cursor()
 
-    sql = "SELECT ID_CAD, NOME_USUARIO, EMAIL FROM CADASTRO WHERE EMAIL = %s AND SENHA = %s"
+    sql = "SELECT cadastro.ID_CAD, NOME_USUARIO, EMAIL, max(compras.ID_PLANO) as plano FROM CADASTRO join compras on cadastro.id_cad = compras.id_cad WHERE cadastro.EMAIL = %s AND cadastro.SENHA = %s"
     val = (email, senha)
     cursor.execute(sql, val)
     login = cursor.fetchone()
     conex.close()
 
     if login:
+        print('Maria Luzia:', login)
         return {'erro': False, 'mensagem': login}
 
     else:
