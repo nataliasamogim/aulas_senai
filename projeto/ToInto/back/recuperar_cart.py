9081060464a707291e48fa8673b804309560b5dc
maria luiza goulart <marialuizagp.senai@gmail.com>
2024-10-01 09:29:16 -0300

    Mudança do formato da data de vencimento

--- projeto/ToInto/back/recuperar_cart.py
+++ projeto/ToInto/back/recuperar_cart.py
@@ -4,7 +4,7 @@ def recuperar_inf_cart(id_cad):
     conex = conexao.conectar()
     cursor = conex.cursor()
 
-    sql = 'SELECT ID_DADOS_PAG, TIPO_PAG, CPF, DATA_PAG, NUM_CARTAO, CVV, DATE_FORMAT(DATA_VENC,"%Y-%m-%d") as DATA_VENC, NOME_CARTAO FROM DADOS_PAG WHERE ID_CAD = %s order by ID_DADOS_PAG desc LIMIT 1'
+    sql = 'SELECT ID_DADOS_PAG, TIPO_PAG, CPF, DATA_PAG, NUM_CARTAO, CVV, DATA_VENC, NOME_CARTAO FROM DADOS_PAG WHERE ID_CAD = %s order by ID_DADOS_PAG desc LIMIT 1'
     val = (id_cad,)
     cursor.execute(sql, val)
     cartao = cursor.fetchone()
@@ -12,7 +12,6 @@ def recuperar_inf_cart(id_cad):
     print(cartao)
 
     if cartao:
-        print('Compra do usuário Laura')
         return {'erro': False, 'mensagem': cartao}
 
     else:
