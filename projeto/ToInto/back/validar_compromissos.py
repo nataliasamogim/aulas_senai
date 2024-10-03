2829d65471c01fd16d7d1c7ee2acf6e0c89e6392
marilia martins bellini <mariliamb06@gmail.com>
2024-10-03 09:24:31 -0300

    alteração validar compromisso python
    tratei as mensagens retornadas para json dentro de um json, assim podendo trabalhar melhor na hora de aparecer as mensagens em baixo dos inputs

--- projeto/ToInto/back/validar_compromissos.py
+++ projeto/ToInto/back/validar_compromissos.py
@@ -1,13 +1,13 @@
 def validar_titulo(titulo):
     if len(titulo) < 3:
-        return {'erro': True, 'mensagem': 'O titulo deve ter pelo menos 3 caracteres'}
+        return {'erro': True, 'mensagem': {'titulo': 'O titulo deve ter pelo menos 3 caracteres'}}
     if len(titulo) > 20:
-        return {'erro' : True, 'mensagem': 'O titulo deve conter no máximo 20 caracteres'}
+        return {'erro' : True, 'mensagem': {'titulo': 'O titulo deve conter no máximo 20 caracteres'}}
 
     return {'erro': False, 'mensagem': ''}
 
 def validar_descricao(desc):
     if len(desc) > 70:
-        return {'erro' : True, 'mensagem': 'A descrição deve conter no máximo 70 caracteres'}
+        return {'erro' : True, 'mensagem': {'descricao':'A descrição deve conter no máximo 70 caracteres'}}
 
     return {'erro': False, 'mensagem': ''}
