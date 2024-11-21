9c6b0b5a54a437d9649c7d1c7e72ba55894c0f16
natalia samogim <samogimnatalia@gmail.com>
2024-11-21 13:58:09 -0300

    Adicionou o checkbox no select
    Checkbox no select do semana e do importante

--- projeto/ToInto/back/recuperar_comp.py
+++ projeto/ToInto/back/recuperar_comp.py
@@ -21,7 +21,7 @@ def recuperar_inf_semana(id_cad, data_in, data_fim):
     conex = conexao.conectar()
     cursor = conex.cursor()
 
-    sql = 'SELECT ID_COMP, TITULO_COMP, DATE_FORMAT(HORARIO_COMP,"%H:%i")AS HORARIO_COMP, DATE_FORMAT(DATA_COMP, "%Y-%m-%d") AS DATA_COMP, DESCRICAO, IMPORTANTE, LEMBRETE FROM COMPROMISSOS WHERE ID_CAD = %s AND DATA_COMP >= DATE_FORMAT(%s, "%Y-%m-%d") and DATA_COMP <= DATE_FORMAT(%s, "%Y-%m-%d") order by DATA_COMP, HORARIO_COMP'
+    sql = 'SELECT ID_COMP, CHECKBOX, TITULO_COMP, DATE_FORMAT(HORARIO_COMP,"%H:%i")AS HORARIO_COMP, DATE_FORMAT(DATA_COMP, "%Y-%m-%d") AS DATA_COMP, DESCRICAO, IMPORTANTE, LEMBRETE FROM COMPROMISSOS WHERE ID_CAD = %s AND DATA_COMP >= DATE_FORMAT(%s, "%Y-%m-%d") and DATA_COMP <= DATE_FORMAT(%s, "%Y-%m-%d") order by DATA_COMP, HORARIO_COMP'
     val = (id_cad,data_in, data_fim)
     cursor.execute(sql, val)
     compromisso = cursor.fetchall()
@@ -37,7 +37,7 @@ def recuperar_inf_semana(id_cad, data_in, data_fim):
 def recuperar_inf_importante(id_cad):
     conex = conexao.conectar()
     cursor = conex.cursor()
-    sql = 'SELECT ID_COMP, TITULO_COMP, DATE_FORMAT(HORARIO_COMP, "%H:%i") AS HORARIO_COMP, DESCRICAO, IMPORTANTE, LEMBRETE, DATE_FORMAT(DATA_COMP, "%Y-%m-%d") AS DATA_COMP FROM COMPROMISSOS WHERE ID_CAD = %s AND IMPORTANTE = 1 AND DATA_COMP >= CURDATE() ORDER BY DATA_COMP, HORARIO_COMP LIMIT 10'
+    sql = 'SELECT ID_COMP, CHECKBOX, TITULO_COMP, DATE_FORMAT(HORARIO_COMP, "%H:%i") AS HORARIO_COMP, DESCRICAO, IMPORTANTE, LEMBRETE, DATE_FORMAT(DATA_COMP, "%Y-%m-%d") AS DATA_COMP FROM COMPROMISSOS WHERE ID_CAD = %s AND IMPORTANTE = 1 AND DATA_COMP >= CURDATE() ORDER BY DATA_COMP, HORARIO_COMP LIMIT 10'
     val = (id_cad,)  # Removido o segundo valor
     cursor.execute(sql, val)
     compromisso = cursor.fetchall()
