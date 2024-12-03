4d339763ee957872ea3814e5c10bc3417bfc3131
maria luiza goulart <marialuizagp.senai@gmail.com>
2024-12-03 12:11:33 -0300

    Comunicação para levar os dados do pix para o backend no Mobile

--- projeto/ToInto/Mobile/pages/Pagina pix/Pix.js
+++ projeto/ToInto/Mobile/pages/Pagina pix/Pix.js
@@ -7,6 +7,40 @@ const Pix = ({ route, navigation }) => {
     // Verifica se o parâmetro code foi passado, se não gera um automaticamente
     const code = route.params?.code || Math.floor(1000 + Math.random() * 9000); // Gera número de 4 dígitos
 
+    const handleSalvarPix = async () => {
+        const id_cad = await AsyncStorage.getItem('ID');
+        const opc_cad = await AsyncStorage.getItem('opc');
+    
+        const dadosSalvarPix = {
+          acao: 'salvar_pix',
+          id: id_cad,
+          escolha_pag: '1',
+          plano_esc: opc_cad,
+          codigo: code
+        };
+    
+        try {
+          const response = await fetch('http://10.135.60.34:8085/receber-dados', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dadosSalvarPix)
+          });
+          const resultado = await response.json();
+    
+          if (resultado.erro) {
+            setMensagensErro(resultado.mensagens);
+            setShowErrorModal(true); // Exibir modal de erro
+          } else {
+            console.log('Dados criados com sucesso!');
+            navigation.navigate('ConfPix');
+          }
+        } catch (error) {
+          console.error('Erro ao receber ao enviar dados do pix:', error);
+        }
+      };
+
     return (
         <KeyboardAvoidingView style={styles.background}>
             <View style={styles.containerPix}>
@@ -19,7 +53,7 @@ const Pix = ({ route, navigation }) => {
             </View>
 
             <View style={styles.btnContPix}>
-                <TouchableOpacity style={styles.btnSubSalPix} onPress={() => navigation.navigate('ConfPix')}>
+                <TouchableOpacity style={styles.btnSubSalPix} onPress={() => handleSalvarPix()}>
                     <Text style={styles.subTxtSalvarPix}>Continuar</Text>
                 </TouchableOpacity>
             </View>
