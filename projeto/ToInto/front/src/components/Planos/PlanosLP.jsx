304dcc9ae94d5ca6537e6ea2ae3a480452ea121b
maria luiza goulart <marialuizagp.senai@gmail.com>
2024-12-10 15:17:34 -0300

    Mudança da descrição dos planos no desktop

--- projeto/ToInto/front/src/components/Planos/PlanosLP.jsx
+++ projeto/ToInto/front/src/components/Planos/PlanosLP.jsx
@@ -24,7 +24,7 @@ function PlanosLP() {
                 <div className="plano_free">
                     <img className="free" src="image/free.png" alt="" />
                     <h1 className="h1_1">Free</h1>
-                    <p className="p1">Use o TOINTO de forma gratuita <br /> com funcionalidades básicas</p>
+                    <p className="p1">Sem acesso aos itens do menu, apenas com acesso ao calendário e um limite de 7 compromissos a serem adicionados por dia.</p>
                     <h1 className="preco_1">R$00,00</h1>
                 </div>
 
@@ -32,7 +32,7 @@ function PlanosLP() {
                 <div className="plano_mensal">
                     <img className="oi" src="image/oi.png" alt="" />
                     <h1 className="h1_2">Plano Mensal</h1>
-                    <p className="p2">Tenha a maioria das funcionalidades <br /> pagando um pequeno valor mensal</p>
+                    <p className="p2">Acesso ao item Hoje e Semana do menu e com um limite de 100 compromissos ao dia.</p>
                     <h1 className="preco_2">R$7,90/mês</h1>
                 </div>
 
@@ -40,7 +40,7 @@ function PlanosLP() {
                 <div className="plano_anual">
                     <img className="anual" src="image/anual.png" alt="" />
                     <h1 className="h1_3">Plano Anual</h1>
-                    <p className="p3">Use todas as funcionalidades <br /> pagando um plano anual</p>
+                    <p className="p3">Acesso a todos itens do menu e compromissos ilimitados.</p>
                     <h1 className="preco_3">R$109,90/ano</h1>
                 </div>
             </section>
